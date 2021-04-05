e2e0b9366a347cd83957200fa86d823e865ab961
sai ganesh <saiganesh@sais-MacBook-Pro.local>
2021-04-05 02:51:19 -0400

    Completed Part - 3

--- Part3/testplugin.php
+++ Part3/testplugin.php
@@ -4,9 +4,36 @@ Plugin Name: testplugin
 
 */
 
+//[promocode code='QWERTY']
 
-
-
-
+function generate_promo($atts){
+            
+    extract(shortcode_atts(array('code' => 'AFGHWQ'), $atts));
+        
+    $post_date = $post_object->post_date; 
+        
+        
+    $post_upd = strtotime($post_date);
+    
+    
+    $today = time();
+    
+    $gap = $today - $post_upd;
+    
+    if($gap > 36000000000){
+    
+        return " Promo Expired";
+    
+    }
+    else {
+    
+        return " $code Code is Valid ";
+    }
+    
+    }
+    
+    add_shortcode('promocode','generate_promo');
+    
+    
 
 ?>
